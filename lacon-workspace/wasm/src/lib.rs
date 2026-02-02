68074dcfb171d868efd0c7b19df6dc52accd60ea
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-02-02 09:27:57 +0200

    WebAssembly addition and workspace changes

---
+++ lacon-workspace/wasm/src/lib.rs
@@ -0,0 +1,25 @@
+use core::frontend::lexer::scanner::Scanner;
+use js_sys::{Array, Object, Reflect};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn lex(source: &str) -> JsValue {
+	let mut scanner = Scanner::new(source.to_string());
+	let tokens = scanner.scan_tokens();
+
+	let result = Array::new();
+
+	for token in tokens.iter() {
+		let obj = Object::new();
+
+		Reflect::set(&obj, &"token_type".into(), &format!("{:?}", token.token_type).into()).unwrap();
+		Reflect::set(&obj, &"lexeme".into(), &token.lexeme.clone().into()).unwrap();
+		Reflect::set(&obj, &"literal".into(), &token.literal.clone().into()).unwrap();
+		Reflect::set(&obj, &"position".into(), &token.position.to_string().into()).unwrap();
+		Reflect::set(&obj, &"flags".into(), &(token.flags.bits() as u32).into()).unwrap();
+
+		result.push(&obj);
+	}
+
+	result.into()
+}
