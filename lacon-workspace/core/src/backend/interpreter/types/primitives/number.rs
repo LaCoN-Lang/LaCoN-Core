61da710e07318623e5c36e581e5dd05225fd607b
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2026-02-04 07:47:33 +0200

    Workspace updates

--- lacon-workspace/core/src/backend/interpreter/types/primitives/number.rs
+++ lacon-workspace/core/src/backend/interpreter/types/primitives/number.rs
@@ -22,7 +22,33 @@ pub enum Number {
 }
 
 impl Number {
-	pub fn match_if_string_equals_to_subtype_name(&self, name: &str) -> bool {
+	pub fn match_type_name(&self, name: &str) -> bool {
 		self.as_ref() == name
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn test_match_if_string_equals_to_subtype_name() {
+		let num = Number::Int32(42);
+		let result1 = num.match_type_name("Int32");
+		println!("Comparing '{}' with 'Int32': {}", num.as_ref(), result1);
+		assert!(result1);
+
+		let result2 = num.match_type_name("Int64");
+		println!("Comparing '{}' with 'Int64': {}", num.as_ref(), result2);
+		assert!(!result2);
+
+		let stringname = "Float64";
+		let num2 = Number::Float64(3.14);
+		let result3 = num2.match_type_name(stringname);
+		println!("Comparing '{}' with '{}': {}", num2.as_ref(), stringname, result3);
+		assert!(result3);
+
+		let result4 = num2.match_type_name("Int16");
+		println!("Comparing '{}' with 'Int16': {}", num2.as_ref(), result4);
+		assert!(!result4);
 	}
 }
